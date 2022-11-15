fe25c77904aaf5d3e647e2b52fc57806d8ac3e93
AlexanderLuz <saveli.gulas@gmail.com>
2022-11-15 22:56:12 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     }
 
+    // Methods for Creating new Students
     public static void instantiateStudents() {
         System.out.println("Enter amount of Students: ");
         int amountOfStudents = Integer.parseInt(askForInput());
@@ -30,9 +31,13 @@ public class Main {
             printStatus(student);
         }
     }
+
+    // Commonly used Methods
     public static String askForInput() {
         return scanner.nextLine();
     }
+
+    // Login and Action procedure
     public static Student studentLogin() {
         System.out.println("Student Login System");
         ConsoleOutputFunctions.printFiller(25, "-");
@@ -64,22 +69,8 @@ public class Main {
         }
         while(!askForContinue());
     }
-    public static void printCoursesWithStatus(Student student) {
-        if(student.coursesEnrolled.size() == 0) {
-            System.out.println("You are not enrolled in any course!");
-        }
-        else {
-            System.out.println("You are enrolled in the courses:");
-            for(Courses course:student.coursesEnrolled) {
-                System.out.println();
-                System.out.print(course.getIndex()+". ");
-                System.out.printf("%-20s", course.getName());
-                System.out.printf("%10s", student.coursesPaymentStatus.get(course).getName());
-                System.out.print(" | Cost: "+course.getCost()+"£");
-            }
-        }
-        System.out.println();
-    }
+
+    // Action Methods
     public static void enrollInNewCourse(Student student) {
         System.out.println("What course do you want to enroll in?");
         int answer = Integer.parseInt(askForInput());
@@ -88,11 +79,6 @@ public class Main {
             student.coursesPaymentStatus.put(Courses.getEnum(answer), Status.TUITION_NOT_PAYED);
             System.out.println("Successfully enrolled in Course: "+Courses.getEnum(answer).getName());
         }
-    }
-    public static void printStatus(Student student) {
-        System.out.println(student.name);
-        System.out.println("Year: "+student.year.getName()+" | Balance: "+student.balance+"£");
-        System.out.println("ID: "+University.IDList.get(student.name));
     }
     public static void payForCourse(Student student) {
         printCoursesWithStatus(student);
@@ -123,4 +109,28 @@ public class Main {
         int answer = Integer.parseInt(askForInput());
         return answer == 1;
     }
+
+    // Print Methods
+    public static void printStatus(Student student) {
+        System.out.println(student.name);
+        System.out.println("Year: "+student.year.getName()+" | Balance: "+student.balance+"£");
+        System.out.println("ID: "+University.IDList.get(student.name));
+    }
+    public static void printCoursesWithStatus(Student student) {
+        if(student.coursesEnrolled.size() == 0) {
+            System.out.println("You are not enrolled in any course!");
+        }
+        else {
+            System.out.println("You are enrolled in the courses:");
+            for(Courses course:student.coursesEnrolled) {
+                System.out.println();
+                System.out.print(course.getIndex()+". ");
+                System.out.printf("%-20s", course.getName());
+                System.out.printf("%10s", student.coursesPaymentStatus.get(course).getName());
+                System.out.print(" | Cost: "+course.getCost()+"£");
+            }
+        }
+        System.out.println();
+    }
+
 }
