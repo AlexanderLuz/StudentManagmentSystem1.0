c0779af01dcb5edaa7625b0ce99bbabd4f9d1d58
AlexanderLuz <saveli.gulas@gmail.com>
2022-11-18 13:56:31 +0100

    Fixed typos

--- src/main/java/Student_Viewmodel.java
+++ src/main/java/Student_Viewmodel.java
@@ -3,7 +3,7 @@ import java.io.Serializable;
 public class Student_Viewmodel implements Serializable {
     public void printStatus(Student student) {
         System.out.println(student.name);
-        System.out.println("Year: "+student.year.getName()+" | Balance: "+student.balance+"£"+" | Bank Balance: "+student.bankBalance);
+        System.out.println("Year: "+student.year.getName()+" | Balance: "+student.balance+"£"+" | Bank Balance: "+student.bankBalance+"£");
         System.out.println("ID: "+Main.sng.UniversityManagement.University.IDList.get(student.name));
         for(Courses course:student.coursesEnrolled) {
             course.printCourseInfo();
